a89b0a8cd9481374250fb9f169f5bf7b76aaebf1
Aldana Laurito <aldana.laurito.AL@gmail.com>
2024-12-28 20:36:49 +0100

    Corrected eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,31 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules"], // Ignore node_modules directory
+  },
+  {
+    plugins: {
+      jest, // Include the Jest plugin
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Include browser globals
+        ...globals.node, // Include Node.js globals (optional)
+        ...globals.jest,
+      },
+        parserOptions: {
+              ecmaVersion: 2018, // Use ECMAScript 2018 features
+            },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      ...js.configs.recommended.rules, // Use recommended rules for JavaScript
+      ...jest.configs.recommended.rules, // Use recommended Jest rules
+    },
+  },
 ];
